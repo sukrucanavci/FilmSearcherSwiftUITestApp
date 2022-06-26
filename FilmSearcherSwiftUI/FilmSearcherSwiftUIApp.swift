273f437fade274ffad803aca78a230c4fba47e2e
sukrucanavci <sukrucanavci53@gmail.com>
2022-06-26 17:01:58 +0300

    Initial Commit

---
+++ FilmSearcherSwiftUI/FilmSearcherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmSearcherSwiftUIApp.swift
+//  FilmSearcherSwiftUI
+//
+//  Created by Şükrü Can Avcı on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FilmSearcherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
